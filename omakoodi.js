97a8ec9843849f212fe9da98f71096630f5ba696
Sanivu86 <59826845+Sanivu86@users.noreply.github.com>
2020-02-07 16:49:26 +0200

    Etusivu

---
+++ omakoodi.js
@@ -0,0 +1,10 @@
+function nayta()
+{
+    var click = document.getElementById("drop-content");
+
+    if(click.style.display ==="none")
+    {
+      click.style.display = "block";
+    }
+    else click.style.display = "none";
+}
